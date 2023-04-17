0fb0908fbe768ddf158c8c3ecd4af822cc552a73
SravaniMygapula <115531569+SravaniMygapula@users.noreply.github.com>
2023-04-17 23:11:05 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,12 @@
+LEFT_EYE_HORIZONTAL_EXTREMES=[36,39]
+LEFT_EYE_TOP=[37,38]
+LEFT_EYE_BOTTOM=[41,40]
+
+RIGHT_EYE_HORIZONTAL_EXTREMES=[42,45]
+RIGHT_EYE_TOP=[43,44]
+RIGHT_EYE_BOTTOM=[47,46]
+
+LEFT_LETTERS=['E','N','I','H','R','Right', 'U','C','W','123','Y','B','V','J','X']
+RIGHT_LETTERS=['T','A','O','S','D','Left','L','M','F','123','G','P','K','Q','Z']
+NUMBERS=['1','2','3','4','5',"Left",'6','7','8',"Right",'9','0','*','#','$']
+FPS=25
